ada9e5b9ae8a01aa88bb6beacb0a74b6cdc29852
Felipe Coury <felipe.coury@gmail.com>
2022-07-07 22:30:28 -0300

    refactor: extracted command parser

--- src/handler.rs
+++ src/handler.rs
@@ -47,28 +47,34 @@ pub fn handle(request_id: u32, op_code: OpCode) -> Result<Vec<u8>, UnknownComman
     }
 }
 
+fn run(doc: Document) -> Result<Document, UnknownCommandError> {
+    let command = doc.keys().next().unwrap();
+
+    println!("******\n*** Command: {}\n******\n", command);
+
+    if command == "isMaster" {
+        IsMaster::new().handle(doc)
+    } else if command == "listDatabases" {
+        ListDatabases::new().handle(doc)
+    } else if command == "find" {
+        Find::new().handle(doc)
+    } else if command == "insert" {
+        Insert::new().handle(doc)
+    } else {
+        println!("Got unknown command: {}", command);
+        Err(UnknownCommandError)
+    }
+}
+
 fn route(msg: &OpCode) -> Result<Document, UnknownCommandError> {
     match msg {
         OpCode::OpMsg(msg) => {
             let doc = msg.sections[0].documents[0].clone();
-            let command = doc.keys().next().unwrap();
-            println!("******\n*** Command: {}\n******\n", command);
-            if command == "isMaster" {
-                IsMaster::new().handle(doc)
-            } else if command == "listDatabases" {
-                ListDatabases::new().handle(doc)
-            } else if command == "find" {
-                Find::new().handle(doc)
-            } else if command == "insert" {
-                Insert::new().handle(doc)
-            } else {
-                println!("Got unknown command: {}", command);
-                Err(UnknownCommandError)
-            }
+            run(doc)
         }
         OpCode::OpQuery(query) => {
             println!("*** Query: {:?}", query);
-            Ok(Document::new())
+            run(query.query.clone())
         }
     }
 }
