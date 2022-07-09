21773184183a2d90ce92caed43d5c51d72c7bba0
Felipe Coury <felipe.coury@gmail.com>
2022-07-09 17:36:07 -0300

    feat: uses local IP Address for whatsmyuri

--- src/commands/whatsmyuri.rs
+++ src/commands/whatsmyuri.rs
@@ -1,6 +1,7 @@
 use crate::handler::CommandExecutionError;
 use crate::{commands::Handler, handler::Request};
 use bson::{doc, Bson, Document};
+use local_ip_address::local_ip;
 
 pub struct WhatsMyUri {}
 
@@ -14,11 +15,13 @@ impl Handler for WhatsMyUri {
         req: &Request,
         _msg: &Vec<Document>,
     ) -> Result<Document, CommandExecutionError> {
-        let socket_addr = &req.peer_addr();
+        let mut peer_addr = req.peer_addr();
+        let my_local_ip = local_ip().unwrap();
+        peer_addr.set_ip(my_local_ip);
 
         Ok(doc! {
           "ok": Bson::Double(1.0),
-          "you": socket_addr.to_string(),
+          "you": peer_addr.to_string(),
         })
     }
 }
