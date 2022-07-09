14c6c1dc988d20ec4cbd5d038b3322f81b0e6873
Felipe Coury <felipe.coury@gmail.com>
2022-07-09 14:46:41 -0300

    feat: added nameOnly optional param to listDatabases

--- src/commands/list_databases.rs
+++ src/commands/list_databases.rs
@@ -9,7 +9,9 @@ impl Handler for ListDatabases {
         ListDatabases {}
     }
 
-    fn handle(&self, _doc: &Vec<Document>) -> Result<Document, CommandExecutionError> {
+    fn handle(&self, docs: &Vec<Document>) -> Result<Document, CommandExecutionError> {
+        let doc = &docs[0];
+        let name_only = doc.get_bool("nameOnly").unwrap_or(false);
         let mut client = PgDb::new();
 
         let mut total_size: i64 = 0;
@@ -18,22 +20,33 @@ impl Handler for ListDatabases {
             if schema.starts_with("pg_") || schema == "information_schema" {
                 continue;
             }
-            let mut size: i64 = 0;
-            for table in client.get_tables(&schema) {
-                let db_size = client.get_table_size(&schema, &table);
-                size += db_size;
-                total_size += db_size;
+            if name_only {
+                databases.push(doc!["name": schema].into());
+            } else {
+                let mut size: i64 = 0;
+                for table in client.get_tables(&schema) {
+                    let db_size = client.get_table_size(&schema, &table);
+                    size += db_size;
+                    total_size += db_size;
+                }
+                let empty = size <= 0;
+                databases.push(doc!["name": schema, "sizeOnDisk": size, "empty": empty].into());
             }
-            let empty = size <= 0;
-            databases.push(doc!["name": schema, "sizeOnDisk": size, "empty": empty].into());
         }
 
         let databases_doc = Bson::Array(databases);
-        Ok(doc! {
-          "databases": databases_doc,
-          "totalSize": Bson::Int64(total_size),
-          "totalSizeMb": Bson::Int64(total_size/1024/1024),
-          "ok": Bson::Double(1.0),
-        })
+        if name_only {
+            Ok(doc! {
+                "databases": databases_doc,
+                "ok": Bson::Double(1.0),
+            })
+        } else {
+            Ok(doc! {
+                "databases": databases_doc,
+                "totalSize": Bson::Int64(total_size),
+                "totalSizeMb": Bson::Int64(total_size/1024/1024),
+                "ok": Bson::Double(1.0),
+            })
+        }
     }
 }
