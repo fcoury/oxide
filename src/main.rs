246b3cc832bbbdc6cdd372a0c04d58fe93926abc
Felipe Coury <felipe.coury@gmail.com>
2022-07-10 15:07:05 -0300

    feat: default log level to INFO

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,9 @@ struct Args {
 
 fn main() {
     dotenv::dotenv().ok();
-    env_logger::init();
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
 
     let args = Args::parse();
     let ip_addr = args.listen_addr.unwrap_or("127.0.0.1".to_string());
