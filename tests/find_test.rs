81ba0a444ad125fe84e20ce2736cd9e94ff76d37
Felipe Coury <felipe.coury@gmail.com>
2022-07-10 00:05:11 -0300

    test: added failing test for insert_many

---
+++ tests/find_test.rs
@@ -0,0 +1,19 @@
+use mongodb::bson::doc;
+
+mod common;
+
+#[test]
+fn basic_find_test() {
+    let ctx = common::setup();
+
+    ctx.col()
+        .insert_many(vec![doc! { "x": 1 }, doc! { "x": 2 }], None)
+        .unwrap();
+
+    let mut cursor = ctx.col().find(None, None).unwrap();
+    let row1 = cursor.next().unwrap().unwrap();
+    assert_eq!(row1.get_i32("x").unwrap(), 1);
+    let row2 = cursor.next().unwrap().unwrap();
+    assert_eq!(row2.get_i32("x").unwrap(), 2);
+    assert!(cursor.next().is_none());
+}
