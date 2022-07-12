66e5328ed34f9f4cb43e97c7a92b4e745bf4fe2f
Felipe Coury <felipe.coury@gmail.com>
2022-07-12 14:02:09 -0300

    feat: adds unary operators like $not

--- src/parser.rs
+++ src/parser.rs
@@ -2,7 +2,8 @@
 use crate::serializer::PostgresSerializer;
 use bson::{Bson, Document};
 use mongodb_language_model::{
-    Clause, Expression, ExpressionTreeClause, LeafClause, LeafValue, Operator, Value, ValueOperator,
+    Clause, Expression, ExpressionTreeClause, LeafClause, LeafValue, Operator,
+    OperatorExpressionOperator, Value, ValueOperator,
 };
 
 pub fn parse(doc: Document) -> String {
@@ -13,6 +14,7 @@ pub fn parse(doc: Document) -> String {
     let json = bson.into_psql_json();
     let str = serde_json::to_string(&json).unwrap();
     let expression = mongodb_language_model::parse(&str).unwrap();
+    log::debug!("{:#?}", expression);
     parse_expression(expression)
 }
 
@@ -51,23 +53,38 @@ fn parse_leaf(leaf: LeafClause) -> String {
             format!("{} = {}", field, parse_leaf_value(leaf_value))
         }
         Value::Operators(val_operators) => {
-            format!("{} {}", field, parse_value_operators(val_operators))
+            // format!("{} {}", field, parse_value_operators(val_operators))
+            parse_value_operators(val_operators, field)
         }
     }
 }
 
-fn parse_value_operators(val_operators: Vec<Operator>) -> String {
+fn parse_value_operators(val_operators: Vec<Operator>, field: String) -> String {
     let values: Vec<String> = val_operators
         .into_iter()
-        .map(|operator| parse_operator(operator))
+        .map(|operator| parse_operator(operator, field.clone()))
         .collect();
     values.join("")
 }
 
-fn parse_operator(oper: Operator) -> String {
+fn parse_operator(oper: Operator, field: String) -> String {
     match oper {
-        Operator::Value(value_oper) => parse_value_operator(value_oper),
+        Operator::Value(value_oper) => format!("{} {}", field, parse_value_operator(value_oper)),
+        Operator::ExpressionOperator(expr_oper) => parse_expression_operator(expr_oper, field),
         t => unimplemented!("parse_operator - unimplemented {:?}", t),
+    }
+}
+
+fn parse_expression_operator(expr_oper: OperatorExpressionOperator, field: String) -> String {
+    let operators: Vec<String> = expr_oper
+        .operators
+        .into_iter()
+        .map(|operator| parse_operator(operator, field.clone()))
+        .collect();
+    let operators_str = operators.join("");
+    match expr_oper.operator.as_str() {
+        "$not" => format!("NOT ({})", operators_str),
+        t => unimplemented!("parse_expression_operator - unimplemented operator {:?}", t),
     }
 }
 
@@ -83,6 +100,7 @@ fn parse_value_operator(value_oper: ValueOperator) -> String {
     };
     let value = parse_leaf_value(value_oper.value);
     format!("{} {}", operator, value)
+    // OperatorResult::new(format!("{} {}", operator, value))
 }
 
 fn parse_expression_tree(exp_tree: ExpressionTreeClause) -> String {
@@ -184,5 +202,30 @@ mod tests {
     fn test_with_gt_oper() {
         let doc = doc! {"age": {"$gt": 12}};
         assert_eq!(r#"_jsonb->'age' > '12'"#, parse(doc))
+    }
+
+    #[test]
+    fn test_with_simple_unary_not() {
+        let doc = doc! { "age": {"$not": {"$gt": 12 } } };
+        assert_eq!(r#"NOT (_jsonb->'age' > '12')"#, parse(doc))
+    }
+
+    #[test]
+    fn test_with_unary_not() {
+        let doc = doc! { "x": 1, "age": {"$not": {"$gt": 12} }, "y": 2 };
+        assert_eq!(
+            r#"(_jsonb->'x' = '1' AND NOT (_jsonb->'age' > '12') AND _jsonb->'y' = '2')"#,
+            parse(doc)
+        )
+    }
+
+    #[test]
+    fn test_with_unary_not_and_or() {
+        let doc =
+            doc! { "age": {"$not": {"$gt": 12} }, "$or": vec! [ doc!{ "y": 2 }, doc!{ "x": 1 } ]};
+        assert_eq!(
+            r#"(NOT (_jsonb->'age' > '12') AND (_jsonb->'y' = '2' OR _jsonb->'x' = '1'))"#,
+            parse(doc)
+        )
     }
 }
