ed9ec0e6c0d7da247df49c612ac699288e101544
Felipe Coury <felipe.coury@gmail.com>
2022-07-12 16:07:31 -0300

    test: added an integration test for find with $or

--- tests/find_test.rs
+++ tests/find_test.rs
@@ -1,3 +1,4 @@
+use bson::Document;
 use mongodb::bson::doc;
 
 mod common;
@@ -37,4 +38,37 @@ fn test_find_string() {
     let row1 = cursor.next().unwrap().unwrap();
     assert_eq!(row1.get_i32("x").unwrap(), 2);
     assert!(cursor.next().is_none());
+}
+
+#[test]
+fn test_find_with_or() {
+    let ctx = common::setup();
+
+    ctx.col()
+        .insert_many(
+            vec![
+                doc! { "x": 1, "name": "Peter" },
+                doc! { "x": 2, "name": "James" },
+                doc! { "x": 3, "name": "Mary" },
+            ],
+            None,
+        )
+        .unwrap();
+
+    let cursor = ctx
+        .col()
+        .find(
+            doc! { "$or": vec![ doc!{ "name": "Peter" }, doc! { "x": 3 }] },
+            None,
+        )
+        .unwrap();
+    let rows: Vec<Result<Document, mongodb::error::Error>> = cursor.collect();
+    assert_eq!(2, rows.len());
+    assert_eq!(
+        vec!["Peter", "Mary"],
+        rows.into_iter()
+            .filter(|r| r.is_ok())
+            .map(|r| r.unwrap().get_str("name").unwrap().to_string())
+            .collect::<Vec<String>>()
+    );
 }
