08b95af168af545e11bef71a4e1ad7992415850b
Felipe Coury <felipe.coury@gmail.com>
2022-07-13 23:03:41 -0300

    fix: second dropDatabase test when running in parallel

--- tests/drop_database_test.rs
+++ tests/drop_database_test.rs
@@ -4,7 +4,7 @@ mod common;
 
 #[test]
 fn test_drop_database() {
-    let ctx = common::setup(); // setup_with_pg_db("test_drop_database_1");
+    let ctx = common::setup();
     let db = ctx.mongodb().database("test_drop_database_1");
     let col = db.collection::<Document>("test");
 
@@ -22,15 +22,15 @@ fn test_drop_database() {
 
 #[test]
 fn test_drop_inexistent_database() {
-    let ctx = common::setup(); // setup_with_pg_db("test_drop_database_1");
-    let db = ctx.mongodb().database("test_drop_database_1");
+    let ctx = common::setup();
+    let db = ctx.mongodb().database("test_drop_database_2");
 
     let dbs = ctx.mongodb().list_database_names(None, None).unwrap();
     println!("{:?}", dbs);
-    assert!(!dbs.contains(&"test_drop_database_1".to_string()));
+    assert!(!dbs.contains(&"test_drop_database_2".to_string()));
 
     db.drop(None).unwrap();
 
     let dbs = ctx.mongodb().list_database_names(None, None).unwrap();
-    assert!(!dbs.contains(&"test_drop_database_1".to_string()));
+    assert!(!dbs.contains(&"test_drop_database_2".to_string()));
 }
