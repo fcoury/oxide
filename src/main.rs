1a69ca9a00fbb619a693b76a019b84b89ef9319f
Felipe Coury <felipe.coury@gmail.com>
2022-07-15 19:57:48 -0300

    feat: initial subcommands implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{AppSettings, Parser, Subcommand};
 use indoc::indoc;
 use server::Server;
 use std::env;
@@ -14,10 +14,28 @@ pub mod threadpool;
 pub mod utils;
 pub mod wire;
 
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Start OxideDB web interface
+    Web {
+        /// Listening address, defaults to 127.0.0.1
+        #[clap(short, long)]
+        listen_addr: Option<String>,
+
+        /// Listening port, defaults to 8087
+        #[clap(short, long)]
+        port: Option<u16>,
+    },
+}
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
-struct Args {
-    /// Listening address defaults to 127.0.0.1
+#[clap(global_setting(AppSettings::ArgsNegateSubcommands))]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+
+    /// Listening address, defaults to 127.0.0.1
     #[clap(short, long)]
     listen_addr: Option<String>,
 
@@ -37,32 +55,41 @@ struct Args {
 fn main() {
     dotenv::dotenv().ok();
 
-    let args = Args::parse();
-    let log_level = if args.debug { "oxide=debug" } else { "info" };
+    let cli = Cli::parse();
+
+    let log_level = if cli.debug { "oxide=debug" } else { "info" };
     env_logger::init_from_env(
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, log_level),
     );
 
-    let ip_addr = args
-        .listen_addr
-        .unwrap_or(env::var("OXIDE_LISTEN_ADDR").unwrap_or_else(|_| "127.0.0.1".to_string()));
-    let port = args.port.unwrap_or(
-        env::var("OXIDE_PORT")
-            .unwrap_or("27017".to_string())
-            .parse()
-            .unwrap(),
-    );
-    let mut pg_url = args.postgres_url;
-    if pg_url.is_none() {
-        pg_url = env::var("DATABASE_URL").ok();
+    match cli.command {
+        Some(Commands::Web { listen_addr, port }) => println!("{:?}, {:?}", listen_addr, port),
+        None => {
+            start(cli.listen_addr, cli.port, cli.postgres_url);
+        }
     }
-    if let Some(pg_url) = pg_url {
-        Server::new_with_pgurl(ip_addr, port, pg_url).start();
-    } else {
-        log::error!(indoc! {"
-            No PostgreSQL URL specified.
-            Use --postgres-url <url> or env var DATABASE_URL to set the connection URL and try again.
-            For more information use --help.
-        "});
+
+    fn start(listen_addr: Option<String>, port: Option<u16>, postgres_url: Option<String>) {
+        let ip_addr = listen_addr
+            .unwrap_or(env::var("OXIDE_LISTEN_ADDR").unwrap_or_else(|_| "127.0.0.1".to_string()));
+        let port = port.unwrap_or(
+            env::var("OXIDE_PORT")
+                .unwrap_or("27017".to_string())
+                .parse()
+                .unwrap(),
+        );
+        let mut pg_url = postgres_url;
+        if pg_url.is_none() {
+            pg_url = env::var("DATABASE_URL").ok();
+        }
+        if let Some(pg_url) = pg_url {
+            Server::new_with_pgurl(ip_addr, port, pg_url).start();
+        } else {
+            log::error!(indoc! {"
+                    No PostgreSQL URL specified.
+                    Use --postgres-url <url> or env var DATABASE_URL to set the connection URL and try again.
+                    For more information use --help.
+                "});
+        }
     }
 }
