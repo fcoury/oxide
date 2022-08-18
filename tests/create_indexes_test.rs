058dff11112e5778df7ba590b7d8143868836bd1
Felipe Coury <felipe.coury@gmail.com>
2022-08-18 11:28:27 -0300

    fix: create index tests

--- tests/create_indexes_test.rs
+++ tests/create_indexes_test.rs
@@ -11,8 +11,16 @@ fn create_indexes_test() {
     let options = None;
     ctx.col().create_index(model, options).unwrap();
 
-    let mut cursor = ctx.col().list_indexes(None).unwrap();
-    let index = cursor.next().unwrap().unwrap();
+    let cursor = ctx.col().list_indexes(None).unwrap();
+    let indexes = cursor
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|x| x.clone().unwrap())
+        .collect::<Vec<_>>();
+    let index = indexes
+        .iter()
+        .find(|x| x.keys == doc! { "x": 1, "z": 1 })
+        .unwrap();
     assert_eq!(index.keys, doc! { "x": 1, "z": 1 });
 }
 
@@ -20,14 +28,22 @@ fn create_indexes_test() {
 fn create_indexes_test_already_existing() {
     let ctx = common::setup();
 
-    let model = IndexModel::builder().keys(doc! { "x": 1, "z": 1 }).build();
+    let model = IndexModel::builder().keys(doc! { "a": 1, "b": 1 }).build();
     let options = None;
     ctx.col()
         .create_index(model.clone(), options.clone())
         .unwrap();
     ctx.col().create_index(model, options).unwrap();
 
-    let mut cursor = ctx.col().list_indexes(None).unwrap();
-    let index = cursor.next().unwrap().unwrap();
-    assert_eq!(index.keys, doc! { "x": 1, "z": 1 });
+    let cursor = ctx.col().list_indexes(None).unwrap();
+    let indexes = cursor
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|x| x.clone().unwrap())
+        .collect::<Vec<_>>();
+    let index = indexes
+        .iter()
+        .find(|x| x.keys == doc! { "a": 1, "b": 1 })
+        .unwrap();
+    assert_eq!(index.keys, doc! { "a": 1, "b": 1 });
 }
