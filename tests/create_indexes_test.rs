4b298dcdfdacf0ab470386553f21428862eded35
Felipe Coury <felipe.coury@gmail.com>
2022-08-18 11:03:04 -0300

    fix: allow creation of already existing indexes

--- tests/create_indexes_test.rs
+++ tests/create_indexes_test.rs
@@ -15,3 +15,19 @@ fn create_indexes_test() {
     let index = cursor.next().unwrap().unwrap();
     assert_eq!(index.keys, doc! { "x": 1, "z": 1 });
 }
+
+#[test]
+fn create_indexes_test_already_existing() {
+    let ctx = common::setup();
+
+    let model = IndexModel::builder().keys(doc! { "x": 1, "z": 1 }).build();
+    let options = None;
+    ctx.col()
+        .create_index(model.clone(), options.clone())
+        .unwrap();
+    ctx.col().create_index(model, options).unwrap();
+
+    let mut cursor = ctx.col().list_indexes(None).unwrap();
+    let index = cursor.next().unwrap().unwrap();
+    assert_eq!(index.keys, doc! { "x": 1, "z": 1 });
+}
